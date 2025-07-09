06e8605d73c43e42e974402be3a7f3ce28f1657a
rin <mugi238@163.com>
2025-07-09 09:15:55 +0800

    普通用户账号管理

--- src/main/java/com/demo/dronebackend/controller/UserController.java
+++ src/main/java/com/demo/dronebackend/controller/UserController.java
@@ -1,46 +1,24 @@
 package com.demo.dronebackend.controller;
 
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.demo.dronebackend.dto.admin.*;
 import com.demo.dronebackend.model.Result;
 import com.demo.dronebackend.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
-    /*
-    密码登录
-     */
-    @PostMapping("/login-pwd")
-    public Result loginByPassword(@Valid @RequestBody LoginByPswReq req) {
-        return userService.loginByPassword(req);
-    }
 
-    /*
-    修改密码
-     */
-    @PostMapping("/change-pwd")
-    public Result<?> updatePassword(@Valid @RequestBody UpdatePasswordReq req) {
-        return userService.updatePassword(req);
-    }
-    /*
-    忘记密码
-     */
-    @PostMapping("/reset-pwd")
-    public Result<?> resetPassword(@Valid @RequestBody ResetReq req) {
-        return userService.resetPassword(req);
-    }
+    private final UserService userService;
 
     /**
      * 发送验证码
+     * @param req 发送验证码请求体
      */
     @PostMapping("/send-code")
     public Result<?> sendCode(@Valid @RequestBody SendCodeReq req) {
@@ -48,11 +26,49 @@ public class UserController {
     }
 
     /**
+     * 密码登录
+     * @param req 密码登录请求体
+     */
+    @PostMapping("/login-pwd")
+    public Result loginByPassword(@Valid @RequestBody LoginByPswReq req) {
+        return userService.loginByPassword(req);
+    }
+
+    /**
      * 验证码登录
+     * @param req 验证码登录请求体
      */
     @PostMapping("/login-code")
     public Result<?> loginByCode(@Valid @RequestBody LoginByCodeReq req) {
         return userService.loginByCode(req);
+    }
+
+
+    /**
+     * 修改密码
+     * @param req 修改密码请求体
+     */
+    @PostMapping("/change-pwd")
+    public Result<?> updatePassword(@Valid @RequestBody UpdatePasswordReq req) {
+        return userService.updatePassword(req);
+    }
+
+    /**
+     * 忘记密码
+     * @param req 重置密码请求体
+     */
+    @PostMapping("/reset-pwd")
+    public Result<?> resetPassword(@Valid @RequestBody ResetReq req) {
+        return userService.resetPassword(req);
+    }
+
+    /**
+     * 用户登出
+     */
+    @GetMapping("/logout")
+    public Result<?> logout() {
+        StpUtil.logout();
+        return Result.success("登出成功");
     }
 
 }
